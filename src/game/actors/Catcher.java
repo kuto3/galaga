bae680847b7f2d749d6c5fb55533e4ce402306c1
thefrenchydev <demetlegal@gmail.com>
2025-12-19 10:10:16 +0100

    moth

--- src/game/actors/Catcher.java
+++
@@ -1,9 +0,0 @@
-package game.actors;
-
-import utils.Vector2;
-
-public class Catcher extends Ennemy {
-    public Catcher(Vector2 targetPosition, double size) {
-        super(targetPosition, 1, 0.02, "catcher", size, 0.1);
-    }
-}
--- src/game/actors/Ennemy.java
+++ src/game/actors/Ennemy.java
@@ -3,7 +3,6 @@ package game.actors;
 import utils.Vector2;
 
 public class Ennemy extends Entity {
-
     public Ennemy(Vector2 pos, int lives, double speed, String sprite, double size, double lerpSpeed) {
         super(pos, lives, speed, sprite, size, lerpSpeed);
     }
